e13f67b640555750e841274c92f510506eec6d89
alessandroCassani <acassani@studenti.uninsubria.it>
2024-10-21 21:41:09 +0200

    implemented main logic

---
+++ exercise_2/client.py
@@ -0,0 +1,92 @@
+import socket
+from sys import argv
+from exercise_1.template_pb2 import Message, FastHandshake
+from threading import Thread
+
+
+def send_message(conn, m):
+    serialized = m.SerializeToString()
+    conn.sendall(len(serialized).to_bytes(4, byteorder="big"))
+    conn.sendall(serialized)
+
+
+def receive_message(conn, m):
+    msg = m()
+    size = int.from_bytes(conn.recv(4), byteorder="big")
+    data = conn.recv(size)
+    msg.ParseFromString(data)
+    return msg
+
+
+def main():
+    host = None
+    port = None
+    try:
+        if len(argv) > 3:
+            host = argv[1]
+            port = int(argv[2])
+            new_id = int(argv[3])
+        elif len(argv) > 1:
+            port = int(argv[1])
+        else:
+            raise ValueError
+    except:
+        host = host or "127.0.0.1"
+        port = 8080
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print("Connected to the server")
+        
+        if new_id:
+            handshake = FastHandshake(id=new_id,change_id = True)
+            send_message(s, handshake)
+        else:
+            handshake = FastHandshake(change_id = False)
+            send_message(s, handshake)
+        
+        handshake = receive_message(s, FastHandshake)
+        
+        if handshake.change_id:
+            print('new id accepted!')
+            print(f"we are client #{handshake.change_id}")
+            id = handshake.change_id
+        else:
+            print('new id rejected!')
+            id = handshake.id
+            
+        if handshake.error:
+            print(f"Handshake failed")
+            return
+
+        Thread(target=handle_incoming_messages,args=(s,), daemon=True).start()
+        while True:
+            try:
+                data = input("Enter a message: \n")
+                data_chunked = data.split(' ', 1)
+                receiver_id = int(data_chunked[0])
+                message = data_chunked[1]
+            except:
+                error = -1
+                message = "end"
+                msg = Message(fr=id, to=error, msg=message)
+                
+            msg = Message(fr=id, to=receiver_id, msg=message)
+            send_message(s, msg)
+            
+            if message == "end":
+                break
+        
+        print("Closing connection")
+
+
+def handle_incoming_messages(conn):
+    print('waiting for messages...')
+    while True:
+        msg = receive_message(conn, Message)
+        print(f"New message arrived: {msg.msg}")
+
+if __name__ == "__main__":
+    main()
+
+
